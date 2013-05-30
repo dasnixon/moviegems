a434988af3b4b2956e556c813cd1fd7b1d9badb7
Chris Nixon <chris.d.nixon@gmail.com>
2013-05-29 22:58:33 -0700

    The start of moviegems, a site to share and expose your films to the public, especially those that dont have the budgets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moviegems::Application.config.secret_key_base = '2cac0db44314e842ba17b55dc65923a0bf66b3b08d86a3d3006da237bab9cddef2ecf8dd8693e4131c472b7b53c74f6c9374facf77ac2aa6f942685aa5cb8855'
